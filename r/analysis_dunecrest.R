4aa4f2ad716393faacf7d0a6d89b8709aa1aa51b
Michele Tobias <mmtobias@ucdavis.edu>
2024-08-13 12:35:50 -0700

    Create analysis_dunecrest.R

---
+++ r/analysis_dunecrest.R
@@ -0,0 +1,24 @@
+# GOAL: find the dune crests through a transect analysis
+
+
+# Set Up ------------------------------------------------------------------
+
+# libraries
+
+
+# read data
+
+
+
+# Analysis ----------------------------------------------------------------
+
+# construct a baseline from the first and last vertexes of the HWL
+
+# construct the transects: https://github.com/paulhegedus/SampleBuilder/
+
+# sample the DEM at each transect
+
+# calculate the change in slope and find the points of inflection
+
+
+
